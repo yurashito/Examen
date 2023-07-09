f5bd2e1e718aa5d49b53f6f8885c46192aaccfaf
DiaryRz <119029048+DiaryRz@users.noreply.github.com>
2023-07-09 11:40:50 +0200

    base1

--- base.sql
+++ base.sql
@@ -1,5 +1,6 @@
 create database regime;
 use regime;
+
 create table utilisateur(
     idUtilisateur int not null auto_increment primary key ,
     nom varchar(20),
@@ -7,4 +8,26 @@ create table utilisateur(
     genre int ,
     mail varchar(50),
     mdp varchar(14)
+);
+
+create table InfoUtilisateur(
+    IdInfoUtilisateur int not null primary key auto_increment,
+    IdUtilisateur int not null,
+    Poids double,
+    Taille double,
+    foreign key(IdUtilisateur) references  Utilisateur(IdUtilisateur)
+);
+
+create table Objectif(
+    IdObjectif int not null primary key auto_increment,
+    NomObjectif varchar(100)
+);
+
+insert into Objectif(NomObjectif) value('Augmenter Poids'),('Diminuer Poids');
+
+create table Code(
+    IdCode int not null primary key auto_increment,
+    ValeurCode double,
+    MontantCode double,
+    Identifiant int
 );
